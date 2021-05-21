d30b1eb062e50cb614a7793bf83dcac3bee8e8e7
skirios <skiriosfoto@gmail.com>
2021-05-21 19:19:46 +0300

    test add

---
+++ src/test/java/PostmanEchoTest.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+public class PostmanEchoTest {
+    @Test
+    void trainingTestPostmanEcho() {
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("om mani padme hum")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("om mani padme hum"))
+        ;
+
+    }
+}
